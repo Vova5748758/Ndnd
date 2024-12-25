6b864c455fbcda3ba9a31cc032771b5e5d12e67c
Vova5748758 <vovansm334@gmail.com>
2024-12-25 22:30:34 +0300

    Create Support.lua

---
+++ Support.lua
@@ -0,0 +1,20 @@
+--message type: WARNING 
+--[[
+unfortunately there is no creator account as it was deleted please use email 
+Sorry!
+
+Email = hffh65910@gmail.com
+Telegram Bot @Scripthackbot
+
+**message 
+Creator: CDTY0898 
+Use of Foreign Artificial Intelligence: true 
+Using other people's scripts: fasle
+Support :hffh65910@gmail.com
+
+
+
+
+
+Official Script Bodi @Scripthackbot
+--]]
